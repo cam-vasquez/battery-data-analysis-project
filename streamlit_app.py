807772cce03275f7f44a60bad3139d33e3d864e9
cam-vasquez <00065221@uca.edu.sv>
2024-12-27 09:09:08 -0600

    ✨ Añadiendo pagina de incio

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+
 
 
 st.title("🖥️ Inicio")
@@ -7,8 +7,3 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))

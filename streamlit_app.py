2d229ecf8cd5a9ba3b278debb37ce2510e03a838
rodolfo <00082421@uca.edu.sv>
2024-12-27 11:20:03 -0600

    :construction: modify readme

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-
-st.title("🖥️ Inicio")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
